36efd0fcbe259df715215112fac6797780dbd1e8
ChefJ <hiecreo@gmail.com>
2017-12-16 13:24:23 +0800

    Code Update
    更新业务逻辑

--- ChangeTheHash/Form1.Designer.cs
+++ ChangeTheHash/Form1.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnChangeHash = new System.Windows.Forms.Button();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.tbxFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHash = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChangeHash
+            // 
+            this.btnChangeHash.Location = new System.Drawing.Point(278, 40);
+            this.btnChangeHash.Name = "btnChangeHash";
+            this.btnChangeHash.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeHash.TabIndex = 0;
+            this.btnChangeHash.Text = "ChangeHash";
+            this.btnChangeHash.UseVisualStyleBackColor = true;
+            this.btnChangeHash.Click += new System.EventHandler(this.btnChangeHash_Click);
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(278, 10);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "Select";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // tbxFilePath
+            // 
+            this.tbxFilePath.Location = new System.Drawing.Point(73, 12);
+            this.tbxFilePath.Name = "tbxFilePath";
+            this.tbxFilePath.Size = new System.Drawing.Size(199, 21);
+            this.tbxFilePath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "FilePath";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "FileHash";
+            // 
+            // lblHash
+            // 
+            this.lblHash.AutoSize = true;
+            this.lblHash.Location = new System.Drawing.Point(71, 45);
+            this.lblHash.Name = "lblHash";
+            this.lblHash.Size = new System.Drawing.Size(191, 12);
+            this.lblHash.TabIndex = 5;
+            this.lblHash.Text = "Select a file to see it\'s hash.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(365, 129);
+            this.Controls.Add(this.lblHash);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxFilePath);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.btnChangeHash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "ChangeTheHash v0.0.0 by ChefJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnChangeHash;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox tbxFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHash;
     }
 }
 
--- ChangeTheHash/Form1.cs
+++ ChangeTheHash/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,59 @@ namespace ChangeTheHash
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            string inFilePath = "";
+            System.Windows.Forms.OpenFileDialog openFileDialog1 = new System.Windows.Forms.OpenFileDialog
+            {
+                //openFileDialog1.InitialDirectory = "c:\\";
+                Filter = "All files (*.*)|*.*| Files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //此处做你想做的事 ...=openFileDialog1.FileName; 
+                inFilePath = openFileDialog1.FileName + openFileDialog1.Title;
+                try
+                {
+                    tbxFilePath.Text = inFilePath;
+                    lblHash.Text = CalculateMD5(inFilePath);
+
+                }
+                catch (Exception exx)
+                {
+
+                    //MessageBox.Show("您的CMS文件格式可能不正确。");
+                }
+                // LicName = System.IO.Path.GetFileName(tbxStraightPath.Text);
+            }
+        }
+
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        static void MD5Obs(string filename,string obschar)
+        {
+            File.AppendAllText(filename, obschar);
+        }
+
+        private void btnChangeHash_Click(object sender, EventArgs e)
+        {
+            MD5Obs(tbxFilePath.Text, "-");
+            lblHash.Text = CalculateMD5(tbxFilePath.Text);
+
         }
     }
 }
